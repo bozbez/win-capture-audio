e1f9e7e897663042a0381a521bbd2d4b697141e4
Joe Kaushal <joe.kaushal@gmail.com>
2021-09-27 17:21:48 +0100

    Refactor helper into a class

--- src/audio-capture-helper/audio-capture-helper.cpp
+++ src/audio-capture-helper/audio-capture-helper.cpp
@@ -4,10 +4,10 @@
 #include <stdbool.h>
 #include <inttypes.h>
 #include <vector>
+#include <stdexcept>
 
 #include <audiopolicy.h>
 #include <mmdeviceapi.h>
-#include <audioclientactivationparams.h>
 
 #include <wrl/implements.h>
 #include <wil/com.h>
@@ -17,6 +17,7 @@
 
 #include "audio-capture-helper.hpp"
 #include "format-conversion.hpp"
+#include "wil/result_macros.h"
 
 using namespace Microsoft::WRL;
 
@@ -45,9 +46,9 @@ struct CompletionHandler
 	}
 };
 
-static AUDIOCLIENT_ACTIVATION_PARAMS get_params(DWORD pid, bool include_tree)
+AUDIOCLIENT_ACTIVATION_PARAMS AudioCaptureHelper::get_params()
 {
-	auto mode = include_tree
+	auto mode = options.include_tree
 			    ? PROCESS_LOOPBACK_MODE_INCLUDE_TARGET_PROCESS_TREE
 			    : PROCESS_LOOPBACK_MODE_EXCLUDE_TARGET_PROCESS_TREE;
 
@@ -55,13 +56,14 @@ static AUDIOCLIENT_ACTIVATION_PARAMS get_params(DWORD pid, bool include_tree)
 		.ActivationType = AUDIOCLIENT_ACTIVATION_TYPE_PROCESS_LOOPBACK,
 		.ProcessLoopbackParams =
 			{
-				.TargetProcessId = pid,
+				.TargetProcessId = options.pid,
 				.ProcessLoopbackMode = mode,
 			},
 	};
 }
 
-static PROPVARIANT get_propvariant(AUDIOCLIENT_ACTIVATION_PARAMS *params)
+PROPVARIANT
+AudioCaptureHelper::get_propvariant(AUDIOCLIENT_ACTIVATION_PARAMS *params)
 {
 	return {
 		.vt = VT_BLOB,
@@ -73,9 +75,27 @@ static PROPVARIANT get_propvariant(AUDIOCLIENT_ACTIVATION_PARAMS *params)
 	};
 }
 
-static wil::com_ptr<IAudioClient> get_audio_client(DWORD pid, bool include_tree)
+void AudioCaptureHelper::init_format()
 {
-	auto params = get_params(pid, include_tree);
+	auto enumerator =
+		wil::CoCreateInstance<MMDeviceEnumerator, IMMDeviceEnumerator>();
+
+	wil::com_ptr<IMMDevice> device;
+	enumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &device);
+
+	wil::com_ptr<IAudioClient> client;
+	device->Activate(__uuidof(IAudioClient), CLSCTX_INPROC_SERVER, NULL,
+			 client.put_void());
+
+	client->GetMixFormat(wil::out_param(format));
+
+	info("format: ch:%d bps:%lu nbl:%d tag:%d", format->nChannels,
+	     format->nAvgBytesPerSec, format->nBlockAlign, format->wFormatTag);
+}
+
+void AudioCaptureHelper::init_client()
+{
+	auto params = get_params();
 	auto propvariant = get_propvariant(&params);
 
 	wil::com_ptr<IActivateAudioInterfaceAsyncOperation> async_op;
@@ -88,176 +108,95 @@ static wil::com_ptr<IAudioClient> get_audio_client(DWORD pid, bool include_tree)
 	completion_handler.event_finished.wait();
 	THROW_IF_FAILED(completion_handler.activate_hr);
 
-	return completion_handler.client;
-}
-
-static wil::unique_cotaskmem_ptr<WAVEFORMATEX> get_default_mix_format()
-{
-	auto enumerator =
-		wil::CoCreateInstance<MMDeviceEnumerator, IMMDeviceEnumerator>();
-
-	wil::com_ptr<IMMDevice> device;
-	enumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &device);
-
-	wil::com_ptr<IAudioClient> client;
-	device->Activate(__uuidof(IAudioClient), CLSCTX_INPROC_SERVER, NULL,
-			 client.put_void());
-
-	wil::unique_cotaskmem_ptr<WAVEFORMATEX> format;
-	client->GetMixFormat(wil::out_param(format));
-
-	return format;
-}
-
-static wil::unique_cotaskmem_ptr<WAVEFORMATEX>
-setup_audio_client(wil::com_ptr<IAudioClient> client,
-		   const wil::unique_event &event)
-{
-	auto format = get_default_mix_format();
-	info("format pre-init: ch:%d bps:%lu nbl:%d tag:%d", format->nChannels,
-	     format->nAvgBytesPerSec, format->nBlockAlign, format->wFormatTag);
+	client = completion_handler.client;
 
 	client->Initialize(AUDCLNT_SHAREMODE_SHARED,
 			   AUDCLNT_STREAMFLAGS_LOOPBACK |
 				   AUDCLNT_STREAMFLAGS_EVENTCALLBACK,
 			   5 * 10000000, 0, format.get(), NULL);
 
-	client->SetEventHandle(event.get());
-	return format;
+	event_data.create();
+	client->SetEventHandle(event_data.get());
 }
 
-static wil::com_ptr<IAudioCaptureClient>
-get_capture_client(wil::com_ptr<IAudioClient> client)
-{
-	wil::com_ptr<IAudioCaptureClient> capture_client;
-	client->GetService(__uuidof(IAudioCaptureClient),
-			   capture_client.put_void());
-
-	return capture_client;
-}
-
-static int parse_options(capture_options_t *options, int argc, char *argv[])
-{
-	if (argc != 4)
-		return 100;
-
-	options->pid = strtoul(argv[1], NULL, 0);
-	if (options->pid == 0)
-		return 101;
-
-	if (strcmp("include", argv[2]) == 0)
-		options->include_tree = true;
-	else if (strcmp("exclude", argv[2]) == 0)
-		options->include_tree = false;
-	else
-		return 102;
-
-	options->tag = argv[3];
-	if (strlen(options->tag) < 1)
-		return 103;
-
-	return 0;
-}
-
-static bool init_helper_data(audio_capture_helper_context_t *ctx)
+void AudioCaptureHelper::init_data()
 {
 	wchar_t name[MAX_PATH];
-	format_name_tag(name, HELPER_DATA_NAME, ctx->options.tag);
+	format_name_tag(name, HELPER_DATA_NAME, options.tag);
 
-	ctx->data_map = wil::unique_handle(
+	data_map = wil::unique_handle(
 		OpenFileMappingW(FILE_MAP_ALL_ACCESS, FALSE, name));
 
-	if (ctx->data_map == NULL) {
+	if (!data_map) {
 		error("failed to open file mapping with name: %ls", name);
-		return false;
+		return;
 	}
 
-	auto data = MapViewOfFile(ctx->data_map.get(), FILE_MAP_ALL_ACCESS, 0,
-				  0, sizeof(audio_capture_helper_data_t));
+	auto data_void = MapViewOfFile(data_map.get(), FILE_MAP_ALL_ACCESS, 0,
+				       0, sizeof(audio_capture_helper_data_t));
 
-	ctx->data = wil::unique_mapview_ptr<audio_capture_helper_data_t>(
-		(audio_capture_helper_data_t *)data);
+	data = wil::unique_mapview_ptr<audio_capture_helper_data_t>(
+		(audio_capture_helper_data_t *)data_void);
 
-	if (!ctx->data) {
+	if (!data) {
 		error("failed to create file map view");
-		return false;
+		return;
 	}
 
-	ctx->data->speakers = get_obs_speaker_layout(
-		(WAVEFORMATEXTENSIBLE *)ctx->format.get());
-	ctx->data->format =
-		get_obs_format((WAVEFORMATEXTENSIBLE *)ctx->format.get());
-	ctx->data->samples_per_sec = ctx->format->nSamplesPerSec;
-
-	return true;
+	data->speakers =
+		get_obs_speaker_layout((WAVEFORMATEXTENSIBLE *)format.get());
+	data->format = get_obs_format((WAVEFORMATEXTENSIBLE *)format.get());
+	data->samples_per_sec = format->nSamplesPerSec;
 }
 
-static void init_events(audio_capture_helper_context_t *ctx)
+void AudioCaptureHelper::init_events()
 {
 	for (int i = 0; i < NUM_HELPER_EVENTS_TOTAL; ++i) {
 		wchar_t name[MAX_PATH];
-		format_name_tag(name, event_names[i], ctx->options.tag);
+		format_name_tag(name, event_names[i], options.tag);
 
-		ctx->events[i].open(name);
+		events[i].open(name);
 	}
 }
 
-static int init_context(audio_capture_helper_context_t *ctx, int argc,
-			char *argv[])
+void AudioCaptureHelper::init_capture()
 {
+	init_format();
+	init_client();
 
-	int ret = parse_options(&ctx->options, argc, argv);
-	if (ret != 0) {
-		error("failed to parse command line options");
-		return ret;
-	}
-
-	ctx->client =
-		get_audio_client(ctx->options.pid, ctx->options.include_tree);
-
-	ctx->event_data.create();
-	ctx->format = setup_audio_client(ctx->client, ctx->event_data);
-	ctx->capture_client = get_capture_client(ctx->client);
-
-	if (!init_helper_data(ctx)) {
-		error("failed to init helper shmem");
-		return 114;
-	}
-
-	init_events(ctx);
-	return 0;
+	client->GetService(__uuidof(IAudioCaptureClient),
+			   capture_client.put_void());
 }
 
-static void forward_audio_packet(audio_capture_helper_context_t *ctx)
+void AudioCaptureHelper::forward_audio_packet()
 {
-	if (InterlockedCompareExchange(&ctx->data->lock, 1, 0) != 0) {
+	if (InterlockedCompareExchange(&data->lock, 1, 0) != 0) {
 		warn("failed to acquire data lock");
 		return;
 	}
 
 	auto cleanup = wil::scope_exit([&]() {
-		InterlockedExchange(&ctx->data->lock, 0);
-		ctx->events[HELPER_EVENT_DATA].SetEvent();
+		InterlockedExchange(&data->lock, 0);
+		events[HELPER_EVENT_DATA].SetEvent();
 	});
 
-	size_t frame_size = ctx->format->nBlockAlign;
+	size_t frame_size = format->nBlockAlign;
 	size_t frame_size_packed =
-		(ctx->format->wBitsPerSample * ctx->format->nChannels) /
-		CHAR_BIT;
+		(format->wBitsPerSample * format->nChannels) / CHAR_BIT;
 
 	UINT32 num_frames = 0;
-	ctx->capture_client->GetNextPacketSize(&num_frames);
+	capture_client->GetNextPacketSize(&num_frames);
 
 	while (num_frames > 0) {
-		BYTE *data;
+		BYTE *new_data;
 		DWORD flags;
 		UINT64 qpc_position;
 
-		ctx->capture_client->GetBuffer(&data, &num_frames, &flags, NULL,
-					       &qpc_position);
+		capture_client->GetBuffer(&new_data, &num_frames, &flags, NULL,
+					  &qpc_position);
 
-		int cur_packet = ctx->data->num_packets++;
-		ctx->data->timestamp[cur_packet] = qpc_position * 100;
+		int cur_packet = data->num_packets++;
+		data->timestamp[cur_packet] = qpc_position * 100;
 
 		bool silent = flags & AUDCLNT_BUFFERFLAGS_SILENT;
 		for (size_t i = 0; i < num_frames; ++i) {
@@ -265,36 +204,86 @@ static void forward_audio_packet(audio_capture_helper_context_t *ctx)
 			size_t pos_packed = i * frame_size_packed;
 
 			for (size_t j = 0; j < frame_size_packed; ++j) {
-				ctx->data->data[cur_packet][pos_packed + j] =
-					silent ? 0 : data[pos + j];
+				data->data[cur_packet][pos_packed + j] =
+					silent ? 0 : new_data[pos + j];
 			}
 		}
 
-		ctx->data->data_size[cur_packet] =
-			frame_size_packed * num_frames;
-		ctx->data->frames[cur_packet] = num_frames;
+		data->data_size[cur_packet] = frame_size_packed * num_frames;
+		data->frames[cur_packet] = num_frames;
 
-		ctx->capture_client->ReleaseBuffer(num_frames);
-		ctx->capture_client->GetNextPacketSize(&num_frames);
+		capture_client->ReleaseBuffer(num_frames);
+		capture_client->GetNextPacketSize(&num_frames);
 	}
 }
 
-static bool tick(audio_capture_helper_context_t *ctx, int event_id, int *err)
+bool AudioCaptureHelper::tick(int event_id)
 {
 	switch (event_id) {
 	case HELPER_WO_EVENT_SHUTDOWN:
 		info("shutting down");
 		return true;
 	case NUM_HELPER_WO_EVENTS: // event_data
-		forward_audio_packet(ctx);
+		forward_audio_packet();
 		return false;
 	default:
 		error("unexpected event id: %d", event_id);
-		*err = 4;
 		return true;
 	}
 
 	return false;
+}
+
+void AudioCaptureHelper::run()
+{
+	int num_events = 1 + NUM_HELPER_WO_EVENTS;
+
+	std::vector<HANDLE> wait_events;
+	for (int i = 0; i < NUM_HELPER_WO_EVENTS; ++i)
+		wait_events.push_back(events[i].get());
+
+	wait_events.push_back(event_data.get());
+
+	client->Start();
+
+	bool shutdown = false;
+	while (!shutdown) {
+		int event_id = WaitForMultipleObjects(
+			num_events, wait_events.data(), false, INFINITE);
+
+		if (!(event_id >= WAIT_OBJECT_0 &&
+		      event_id < WAIT_OBJECT_0 + num_events)) {
+			error("error waiting for events");
+			shutdown = true;
+
+			break;
+		}
+
+		shutdown = tick(event_id);
+	}
+
+	client->Stop();
+}
+
+CaptureOptions::CaptureOptions(int argc, char *argv[])
+{
+	if (argc != 4)
+		throw std::runtime_error("wrong number of arguments");
+
+	pid = strtoul(argv[1], NULL, 0);
+	if (pid == 0)
+		throw std::runtime_error("failed to parse PID");
+
+	if (strcmp("include", argv[2]) == 0)
+		include_tree = true;
+	else if (strcmp("exclude", argv[2]) == 0)
+		include_tree = false;
+	else
+		throw std::runtime_error("failed to parse mode");
+
+	tag = argv[3];
+	if (strlen(tag) < 1)
+		throw std::runtime_error("failed to parse tag");
 }
 
 int main(int argc, char *argv[])
@@ -305,44 +294,19 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	int ret = 0;
+	try {
+		CaptureOptions options(argc, argv);
+		AudioCaptureHelper helper(options);
 
-	audio_capture_helper_context_t ctx;
-	ret = init_context(&ctx, argc, argv);
-	if (ret != 0) {
-		error("failed to init context");
-		return ret;
+		helper.run();
+	} catch (wil::ResultException err) {
+		error("%s", err.what());
+		return err.GetErrorCode();
+	} catch (std::runtime_error err) {
+		error("%s", err.what());
+		return 1;
 	}
 
-	info("capture initialized");
-
-	int num_events = 1 + NUM_HELPER_WO_EVENTS;
-
-	std::vector<HANDLE> events;
-	for (int i = 0; i < NUM_HELPER_WO_EVENTS; ++i)
-		events.push_back(ctx.events[i].get());
-
-	events.push_back(ctx.event_data.get());
-
-	ctx.client->Start();
-
-	bool shutdown = false;
-	while (!shutdown) {
-		int event_id = WaitForMultipleObjects(num_events, events.data(),
-						      false, INFINITE);
-
-		if (!(event_id >= WAIT_OBJECT_0 &&
-		      event_id < WAIT_OBJECT_0 + num_events)) {
-			error("error waiting for events");
-
-			ret = 2;
-			shutdown = true;
-		}
-
-		shutdown = tick(&ctx, event_id, &ret);
-	}
-
-	ctx.client->Stop();
-
-	return ret;
+	CoUninitialize();
+	return 0;
 }
--- src/audio-capture-helper/audio-capture-helper.hpp
+++ src/audio-capture-helper/audio-capture-helper.hpp
@@ -2,6 +2,7 @@
 
 #include <windows.h>
 #include <audioclient.h>
+#include <audioclientactivationparams.h>
 
 #include <wil/com.h>
 
@@ -18,24 +19,53 @@
 #define debug(format, ...) \
 	do_log("debug: (%s): " format "\n", __func__, ##__VA_ARGS__)
 
-typedef struct capture_options {
+struct CaptureOptions {
 	DWORD pid;
 	bool include_tree;
 
 	char *tag;
-} capture_options_t;
 
-typedef struct audio_capture_helper_context {
-	capture_options_t options;
+	CaptureOptions(int argc, char *argv[]);
+};
+
+class AudioCaptureHelper {
+private:
+	CaptureOptions options;
 
 	wil::com_ptr<IAudioClient> client;
 	wil::com_ptr<IAudioCaptureClient> capture_client;
 
 	wil::unique_cotaskmem_ptr<WAVEFORMATEX> format;
-	
+
 	wil::unique_event event_data;
 	wil::unique_event events[NUM_HELPER_EVENTS_TOTAL];
 
 	wil::unique_handle data_map;
 	wil::unique_mapview_ptr<audio_capture_helper_data_t> data;
-} audio_capture_helper_context_t;
+
+	AUDIOCLIENT_ACTIVATION_PARAMS get_params();
+	PROPVARIANT get_propvariant(AUDIOCLIENT_ACTIVATION_PARAMS *params);
+
+	void init_format();
+	void init_client();
+
+	void init_capture();
+
+	void init_data();
+	void init_events();
+
+	void forward_audio_packet();
+	bool tick(int event_id);
+
+public:
+	AudioCaptureHelper(CaptureOptions options) : options{options} {
+		init_capture();
+
+		init_data();
+		init_events();
+	};
+
+	~AudioCaptureHelper() = default;
+
+	void run();
+};
