ac2da3f9fe4700d455ba32754ef30efc8e8ef267
Joe Kaushal <joe.kaushal@gmail.com>
2022-02-18 22:30:37 +0000

    Log git hash with version

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,6 +1,7 @@
 #include <obs-module.h>
+
 #include "common.hpp"
-#include "plugin-macros.generated.h"
+#include "plugin-macros.generated.hpp"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("win-capture-audio", "en-GB")
@@ -9,7 +10,7 @@ extern struct obs_source_info audio_capture_info;
 
 bool obs_module_load(void)
 {
-	blog(LOG_INFO, "[win-capture-audio] Version %s", PLUGIN_VERSION);
+	blog(LOG_INFO, "[win-capture-audio] Version %s (%s)", PLUGIN_VERSION, GIT_HASH);
 	obs_register_source(&audio_capture_info);
 	return true;
 }
