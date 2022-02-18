203defdaed02e9e63b884754865e50c442d94e24
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2022-02-18 23:22:58 +0100

    Log plugin version (#116)

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,4 +1,6 @@
 #include <obs-module.h>
+#include "common.hpp"
+#include "plugin-macros.generated.h"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("win-capture-audio", "en-GB")
@@ -7,6 +9,7 @@ extern struct obs_source_info audio_capture_info;
 
 bool obs_module_load(void)
 {
+	blog(LOG_INFO, "[win-capture-audio] Version %s", PLUGIN_VERSION);
 	obs_register_source(&audio_capture_info);
 	return true;
 }
