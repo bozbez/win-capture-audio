9b0af1d58656c5a6c4c02f5543ba5f92ee5e11c9
Joe Kaushal <joe.kaushal@gmail.com>
2022-07-04 14:38:58 +0100

    Fix comment typo

--- src/session-monitor.hpp
+++ src/session-monitor.hpp
@@ -202,7 +202,7 @@ public:
 class SessionMonitor {
 private:
 	wil::critical_section callbacks_lock;
-	std::unordered_map<DWORD, std::tuple<UINT, UINT>> callbacks; // cleint - {added, expired}
+	std::unordered_map<DWORD, std::tuple<UINT, UINT>> callbacks; // client - {added, expired}
 
 	wil::critical_section sessions_lock;
 	std::unordered_map<SessionKey, std::string> sessions_list;
